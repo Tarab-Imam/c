10fffe948ba15bf3fc0e2cb6843a4952495b4df5
Tarab Imam <imamtarab@gmail.com>
2024-11-01 19:31:31 +0530

    ADD

---
+++ vowel.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+    char d;
+    printf("enter a character");
+    scanf("%c",&d);
+    switch(d)
+    {
+        case'a':printf("Vowel");break;
+        case'e':printf("Vowel");break;
+        case'i':printf("Vowel");break;
+        case'o':printf("Vowel");break;
+        case'u':printf("Vowel");break;
+        default:printf("consonant");break;
+    }
+}
