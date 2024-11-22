43a1108ef7b66248c66a4bbc1ce086f11043255f
Tarab Imam <imamtarab@gmail.com>
2024-11-22 19:47:23 +0530

    hg

---
+++ array3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+    int A[]={9,6,5,2,4};
+    int temp,i;
+    for (i=0;i<6;i++)
+    {
+        for (j=0;j<6;j++)
+        {
+            if (A[j]>A[j+1])
+            {
+                temp=A[j];
+                A[j]=A[j+1];
+                A[j+1]=temp;
+            }
+            printf("%d",A);
+        }
+    }
+    
+}
