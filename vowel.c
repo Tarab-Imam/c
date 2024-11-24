331e040f1cd1e721b1738be6ec2cc564d8544521
Tarab Imam <imamtarab@gmail.com>
2024-11-24 23:27:06 +0530

    hh

--- vowel.c
+++ vowel.c
@@ -7,7 +7,7 @@ void main()
     switch(d)
     {
         case'a':printf("Vowel");break;
-        case'e':printf("Vowel");break;
+        case'e':printf("Vowel");
         case'i':printf("Vowel");break;
         case'o':printf("Vowel");break;
         case'u':printf("Vowel");break;
